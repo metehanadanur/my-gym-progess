467a99a40438bc2e7f10c01cdc40f7848bfbebdc
Muhammed Metehan Adanur <77002704+metehanadanur@users.noreply.github.com>
2023-05-18 23:57:38 +0200

    PC commit

---
+++ src/ui-components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/nutrition">Nährungswerte</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+
+export default Header
