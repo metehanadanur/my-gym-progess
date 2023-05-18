144d7efd449dce661e1d7f8e2c202c538e9456fc
Muhammed Metehan Adanur <77002704+metehanadanur@users.noreply.github.com>
2023-05-19 00:17:31 +0200

    Adding a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import Header from './ui-components/Header';
+import React from "react";
+import "./App.css";
+import Header from "./ui-components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Header></Header>
+        <Header></Header>
+        <h1 className="text-3xl font-bold underline">My Gym Progress</h1>
       </header>
     </div>
   );
--- src/ui-components/Header.tsx
+++ src/ui-components/Header.tsx
@@ -2,15 +2,24 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="absolute top-0">
+      <ul className="flex justify-between">
+        <li className="bg-blue p-6 hover:bg-sky-700">
+          <a href="/home">Home</a>
+        </li>
+        <li className="bg-blue p-6 bg-header">
           <a href="/nutrition">Nährungswerte</a>
+        </li>
+
+        <li className="bg-blue p-6 hover:bg-sky-700">
+          <a href="/gymplan">Trainingsplan</a>
+        </li>
+        <li className="bg-blue p-6 hover:bg-sky-700">
+          <a href="/progress">Fortschritt</a>
         </li>
       </ul>
     </nav>
   );
 };
 
-
-export default Header
+export default Header;
